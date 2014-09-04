3438673a753b3e9a45dccd2cc345b58d763020c7
Jonathan Bryce <jbryce@jbryce.com>
2014-09-03 22:30:52 -0500

    Fixing my mistake on the spacing with the print function and one last typo

--- python-helloworldbook/tempatureconversion.py
+++ python-helloworldbook/tempatureconversion.py
@@ -1,7 +1,7 @@
-print "This program converts Fahrenhiet to Celsius"
+print "This program converts Fahrenheit to Celsius"
 print "Type in a tempature in Fahrenheit: ",
 fahrenheit = float(raw_input())
 celsius = (fahrenheit - 32) * 5.0 / 9
-print "That is ",
+print "That is",
 print celsius,
-print " degrees Celsius"
+print "degrees Celsius"
