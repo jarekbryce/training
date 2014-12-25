8f1ef11ca9a10916bf3c4c3b98ed2cb3d82830ea
Jarek Bryce <jarek@jbryce.com>
2014-12-25 16:34:24 -0600

    Looping practice

---
+++ python-helloworldbook/first_conditional_loop.py
@@ -0,0 +1,7 @@
+print "Press spacebar to begin."
+someInput = raw_input()
+while someInput == ' ':
+    print "Launching game...please wait."
+    print "Press spacebar to begin."
+    someInput = raw_input()
+print "Waiting..."    
---
+++ python-helloworldbook/loopusingnames.py
@@ -0,0 +1,2 @@
+for cool_guy in ["The Rock","Phil Colson"]:
+    print cool_guy, "is the coolest guy ever!" 
